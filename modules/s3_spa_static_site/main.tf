721f32a13a4f0f6006fd91799560d45f1fb5e911
Glenn Gillen <me@glenngillen.com>
2022-03-28 20:55:48 +1100

    Fixing broken OAI definition.

--- modules/s3_spa_static_site/main.tf
+++ modules/s3_spa_static_site/main.tf
@@ -2,7 +2,7 @@ resource "aws_cloudfront_origin_access_identity" "this" {
   comment = "OAI for s3 website access"
 }
 
-data "aws_iam_policy_document" "s3_website_policy" {
+data "aws_iam_policy_document" "oai_policy_document" {
   statement {
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.this.arn}/*"]
@@ -12,13 +12,21 @@ data "aws_iam_policy_document" "s3_website_policy" {
       identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
     }
   }
-}
 
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.this-dot-com.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
+    }
+  }
+}
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.s3_website_policy.json
+  policy = data.aws_iam_policy_document.oai_policy_document.json
 }
-
 resource "aws_s3_bucket" "this" {
     bucket = var.domain
     acl = "private"
