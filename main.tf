9e5535b37f63ded2cc40b8ffc21fcb029dbadd29
Glenn Gillen <me@glenngillen.com>
2022-03-28 22:18:33 +1100

    Wrong domain init.

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ module "bucket" {
 module "cdn" {
     source = "./modules/cdn"
 
-    domain = var.domain
     domain_aliases = concat(var.domain, var.domain_aliases)
     certificate_arn = var.certificate_arn
     cloudfront_access_identity_path = bucket.cloudfront_access_identity_path
